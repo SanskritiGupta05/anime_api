9e8460233c7427adad96ee8c612befab0b7c529a
Sanskriti Gupta <sanskriti.gupta.005@gmail.com>
2023-02-08 11:38:02 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 4000;
+const fs = require('fs');
+
+app.get('/', (req, res) => {
+  res.end('Hello World!');
+});
+
+app.get("/list_anime", (req, res) => {
+    fs.readFile(__dirname + '/' + 'anime.json', (err, data) => {
+        res.end(data);
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`)
+  });
